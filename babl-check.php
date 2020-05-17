be2eac1624e72b65b0302e1ce60363fb52eda05d
DriedSponge <jortuck0810@gmail.com>
2020-05-16 23:35:57 -0700

    Made some ajustments to styling, made it more responsive, fixed typo.

--- babl-check.php
+++ babl-check.php
@@ -1,7 +1,7 @@
 <?php
 $status = true;
 if (version_compare(phpversion(), '7.2', '<') || version_compare(phpversion(), '7.2.999', '>')) {
-	$php_status = '<span class="text-danger">Your PHP version is not supported. Please switch to 7.1 or 7.2.</span>';
+	$php_status = '<span class="text-danger">Your PHP version is not supported. Please switch to 7.1 or 7.2.<br> (Current Version: '. phpversion() .')</span>';
 	$status = false;
 } else {
 	$php_status = '<span class="text-success">' . phpversion() . '</span>';
@@ -39,6 +39,7 @@ if (function_exists("escapeshellarg")) {
 <head>
 	<base target="_blank">
 	<title>Babl Compatability Checker</title>
+	<meta name="viewport" content="width = device-width, initial-scale = 1">
 	<link href="https://driedsponge.net/resources/bablicon.ico" rel="icon" type="image/icon">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
@@ -58,9 +59,11 @@ if (function_exists("escapeshellarg")) {
 	</style>
 	<div class="container" style="padding-top:10px">
 		<h1>Babl Compatability Checker</h1>
-		<h5><a href="https://github.com/MitchellPT/babl-requirment-checker">Github</a> &bull; <a href="https://www.gmodstore.com/market/view/babl-where-a-community-thrives">Babl</a></h5>
+		<h5><a href="https://github.com/MitchellPT/babl-requirment-checker">Github</a> &bull; <a href="https://www.gmodstore.com/market/view/babl-where-a-community-thrives">Babl</a> &bull; <a href="https://docs.rainn.xyz">Babl Docs</a> </h5>
+		<hr style="background-color: white">
 		<br>
-		<table class="table table-bordered table-dark table-hover">
+		<div class="table-responsive">
+		<table class="table  table-bordered table-dark table-hover" >
 			<thead>
 				<tr>
 					<th scope="col">Name</th>
@@ -70,7 +73,7 @@ if (function_exists("escapeshellarg")) {
 			<tbody>
 				<tr>
 					<td>PHP Version</td>
-					<td><?= htmlspecialchars_decode($php_status); ?></td>
+					<td><b><?= htmlspecialchars_decode($php_status); ?></b></td>
 				</tr>
 				<?php
 				foreach ($extentions as  $k => $v) {
@@ -80,12 +83,12 @@ if (function_exists("escapeshellarg")) {
 						}
 						$ext_status = '<span class="text-danger">Disabled</span>';
 					} else {
-						$ext_status = '<span class="text-success">Eabled</span>';
+						$ext_status = '<span class="text-success">Enabled</span>';
 					}
 				?>
 					<tr>
 						<td><?= htmlspecialchars($k); ?></td>
-						<td> <?= htmlspecialchars_decode($ext_status); ?></td>
+						<td><b><?= htmlspecialchars_decode($ext_status); ?></b></td>
 					</tr>
 
 				<?php
@@ -93,14 +96,15 @@ if (function_exists("escapeshellarg")) {
 				?>
 				<tr>
 					<td>allow_url_fopen</td>
-					<td><?php echo $fopen_status; ?></td>
+					<td><b><?php echo $fopen_status; ?></b></td>
 				</tr>
 				<tr>
 					<td>escapeshellarg (PHP Fucntion)</td>
-					<td><?php echo $escapeshellarg_status; ?></td>
+					<td><b><?php echo $escapeshellarg_status; ?></b></td>
 				</tr>
 			</tbody>
 		</table>
+		</div>
 		<br>
 		<?php
 		if ($status == true) {
